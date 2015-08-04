eb8eb148cbe6076127c3218303db5619a8537f22
Ari & Judy <Ari+Judy@appacademy.io>
2015-08-04 14:07:47 -0400

    Add Board#setup_pieces and Board#populate_row helper

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 class Board
+  attr_reader :grid
 
   def initialize(grid = Array.new(8) { Array.new(8) })
     @grid = grid
@@ -14,5 +15,38 @@ class Board
     @grid[x][y] = value
   end
 
+  def setup_pieces
+    populate_row(0, :W, base_row_pieces)
+    populate_row(1, :W, Array.new(8) { Pawn.new })
+
+    populate_row(7, :B, base_row_pieces.reverse)
+    populate_row(6, :B, Array.new(8) { Pawn.new })
+
+    nil
+  end
+
+  def populate_row(row_idx, color, pieces)
+    grid[row_idx].each_index do |idx|
+      piece = pieces[idx]
+      piece.pos, piece.board, piece.color = [0, idx], self, color
+
+      self[[0,idx]] = piece
+    end
+
+    nil
+  end
+
+  def base_row_pieces
+    [
+      Rook.new,
+      Knight.new,
+      Bishop.new,
+      Queen.new,
+      King.new,
+      Bishop.new,
+      Knight.new,
+      Rook.new
+    ]
+  end
 
 end
