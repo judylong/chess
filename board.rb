1ed9f0a5855a71596599661f88caff5cc1a7d0eb
Ari & Judy <Ari+Judy@appacademy.io>
2015-08-04 13:46:25 -0400

    Add Board#initialize and bracket methods

--- board.rb
+++ board.rb
@@ -1,12 +1,17 @@
 class Board
-  def initialize
+
+  def initialize(grid = Array.new(8) { Array.new(8) })
+    @grid = grid
   end
 
   def [](pos)
-
+    x, y = pos
+    @grid[x][y]
   end
 
   def []=(pos, value)
+    x, y = pos
+    @grid[x][y] = value
   end
 
 
