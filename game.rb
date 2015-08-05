a113935ecf337b0e49a6b346e0a121db25b45122
Ari & Judy <Ari+Judy@appacademy.io>
2015-08-05 12:10:08 -0400

    Initialize game with fully setup board

--- game.rb
+++ game.rb
@@ -1,11 +1,18 @@
+require_relative :board
+
 class Game
+  attr_reader :board
+
   def initialize
+    @board = Board.new
+    @board.setup_pieces
   end
-  
+
   def prompt
   end
 
   def play
+
   end
 
 end
