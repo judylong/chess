992c289d42f0e986e4b16798a7191341ab008530
Ari & Judy <Ari+Judy@appacademy.io>
2015-08-05 12:16:58 -0400

    Add @current_player instance var to Game and add skeleton to Game#play

--- game.rb
+++ game.rb
@@ -1,18 +1,26 @@
 require_relative :board
 
 class Game
-  attr_reader :board
+  attr_reader :board, :current_player
 
   def initialize
     @board = Board.new
     @board.setup_pieces
+    @current_player = :W
+  end
+
+  def play
+    until board.checkmate?(current_player)
+      board.render
+      board.move(*get_move)
+      switch_player
+    end
+
+    board.render
+    switch_player
+    puts "Game over! #{current_player} wins!"
   end
 
   def prompt
   end
-
-  def play
-
-  end
-
 end
